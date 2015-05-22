b07808259199ca06fad313866139197c945f130e
Mori <keitamori@gmail.com>
2015-05-22 14:29:42 +0200

    Ajout de la fonctionnalité d'éditer plusieurs réponses a une question

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   end
 
   get 'questions/:id/delete' => 'questions#destroy', as: :delete_question
+  get 'questions/:id/edit_an_answer' => 'answers#edit_an_answer', as: :edit_answer_for_question
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
